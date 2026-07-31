47f98de595f1df811d4307559cbc311fbbb1b2c7
Andrew-Kurkov <68422355+Andrew-Kurkov@users.noreply.github.com>
2026-07-31 17:45:57 +0400

    Update test_characters.py
    Добавлены негативные проверки получения информации по ИД. Заменена одна позитивная проверка получения информации по ИД на последнего персонажа в списке

--- futuramaApiAutotests/src/api/tests/test_characters.py
+++ futuramaApiAutotests/src/api/tests/test_characters.py
@@ -8,7 +8,7 @@ class TestCharacters:
     client = get_client()
     @pytest.mark.parametrize("character_id, character_name", [(1,"Philip J. Fry"),
                                                                 (2,"Morgan Proctor"),
-                                                                (3,"Mugger")])
+                                                                (427,"Australian man")])
     def test_get_character_by_id(self,character_id,character_name):
         """Проверка получения информации о персонаже по ИД."""
         response = get_character(self.client,character_id)
@@ -16,6 +16,12 @@ class TestCharacters:
         assert response.json()["name"] == character_name
         assert response.json()["id"] == character_id
 
+    @pytest.mark.parametrize("incorrect_id", [-1, 0, 428, "abc"])
+    def test_get_character_by_incorrect_id(self, incorrect_id):
+        """Проверка получения информации о персонаже по-некорректному ИД."""
+        response = get_character(self.client, incorrect_id)
+        assert response.status_code == 404 or response.status_code == 422
+        
     def test_get_charaters_list(self):
         """Проверка получения списка персонажей"""
         response = get_characters_list(self.client)
